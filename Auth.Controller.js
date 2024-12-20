9bbb1ac863dd5e69c525ce4cd51dce0d0886007d
sandhyachan <sandhyachan6@gmail.com>
2024-12-20 19:09:02 +0530

    Add route for password reset request (POST /reset-password)

--- Auth.Controller.js
+++ Auth.Controller.js
@@ -62,3 +62,40 @@ const userLogin = async (request, response) => {
         })
     }
 }
+
+const userResetPassword = async (request, response) => {
+    const EMAIL = request.body.email
+    const PASSWORD = request.body.password
+    
+    const userExist = await UserModel.findOne({ email: EMAIL })
+    
+    if (!userExist) {
+        return response.status(404).json({
+            message: "Account not found! Please enter your registered email!"
+        })
+    }
+
+    if (PASSWORD !== request.body.confirmPassword) {
+        return response.status(400).json({
+            message: "Please make sure your passwords match"
+        })
+    }
+
+    try {
+        await UserModel.updateOne(
+            { _id: userExist._id },
+            { $set: { password: PASSWORD } }
+        )
+
+        response.status(200).json({
+            message: "Password updated successfully"
+        })
+    } catch (error) {
+        console.error("Error during password reset:", error)
+        response.status(500).json({
+            message: "Something went wrong"
+        })
+    }
+}
+
+module.exports = { userSignUp, userLogin, userResetPassword }
