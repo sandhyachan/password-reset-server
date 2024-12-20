2eac9732e4f864010fbe3945e07a9a63cd3c0a19
sandhyachan <sandhyachan6@gmail.com>
2024-12-20 19:06:39 +0530

    Add User schema

---
+++ User.Model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    resetToken: { 
+        type: String, 
+        required: false 
+    },
+    tokenExpiration: { 
+        type: Date, 
+        required: false 
+    }
+})
+
+const UserModel = mongoose.model('users', UserSchema)
+
+module.exports = {UserModel}
