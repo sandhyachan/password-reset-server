5e07da4115ebeed61bc6cb2ba903f3a6c6448353
sandhyachan <sandhyachan6@gmail.com>
2024-12-20 19:06:17 +0530

    Set up config connection using Mongoose

---
+++ dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectDB = async (request, response) => {
+    try {
+        const connection = await mongoose.connect("mongodb+srv://sandhyachan:Password123@cluster0.ahjkd.mongodb.net/ElysianShores?retryWrites=true&w=majority&appName=Cluster0", { serverSelectionTimeoutMS: 30000})
+        if(connection){
+            console.log("DB connection successful")
+        } else {
+            throw new Error("DB connection could not be established")
+        }
+    } catch (error) {
+        console.log("DB connection error:", error.message)
+    }
+}
+
+module.exports = { connectDB }
