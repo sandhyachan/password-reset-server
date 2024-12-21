60f8628fc9021be24167b4edddb944ec29967c02
sandhyachan <sandhyachan6@gmail.com>
2024-12-21 12:01:29 +0530

    Generate reset token and send email to reset credentials

--- controller/Auth.Controller.js
+++ controller/Auth.Controller.js
@@ -1,8 +1,9 @@
 const sgMail = require('@sendgrid/mail')
-const { UserModel } = require("../model/User.Model");
-const crypto = require('crypto');
-const { response } = require('express');
+const { UserModel } = require("../model/User.Model")
+const crypto = require('crypto')
+const { response } = require('express')
 
+require('dotenv').config()
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const generateResetToken = () => {
@@ -10,9 +11,15 @@ const generateResetToken = () => {
 }
 
 const userSignUp = async (request, response) => {
-    if(!request.body.email || !request.body.password || !request.body.fullName || !request.body.phoneNumber){
+    const PASSWORD = request.body.password
+    if(!request.body.email || !PASSWORD || !request.body.fullName || !request.body.phoneNumber){
         return response.status(400).json({
             message: "Please enter all the required fields."
+        })
+    }
+    if(PASSWORD.length<6){
+        return response.status(400).json({
+            message: "Password length must be atleast 6 characters!"
         })
     }
     try {
@@ -72,20 +79,65 @@ const userLogin = async (request, response) => {
     }
 }
 
+const forgotPassword = async (request, response) => {
+    const { email } = request.body
+
+    const user = await UserModel.findOne({ email })
+    if (!user) {
+        return response.status(404).json({ message: 'Account not found!' })
+    }
+
+    const resetToken = generateResetToken()
+    const tokenExpiration = Date.now() + 3600000  // Token expires in 1 hour
+
+    user.resetToken = resetToken
+    user.tokenExpiration = tokenExpiration
+    await user.save()
+
+    const resetLink = `https://localhost:3000/resetpassword`
+
+    const msg = {
+        to: email,
+        from: 'honeycoupleart@gmail.com',
+        subject: 'Password Reset Request',
+        text: `Click the link below to reset your password (this link will expire in 1 hour):\n\n${resetLink}. Copy your reset token from here for verification. ${resetToken}`,
+        html: `
+        <p>Click the link below to reset your password (this link will expire in 1 hour):</p>
+        <a href="${resetLink}">Reset Password</a>
+        <p>Copy your reset token from here for verification.</p>
+        <h6>${resetToken}</h6>`
+    }
+
+    try {
+        console.log('Sending email to:', email)
+
+        await sgMail.send(msg)
+
+        return response.status(200).json({ message: 'Password reset email sent!' })
+    } catch (error) {
+        console.error('Error sending email:', error.response ? error.response.body : error.message)
+        return response.status(500).json({
+            message: 'Failed to send reset email. Please try again later.',
+            error: error.response ? error.response.body : error.message,
+        })
+    }
+}
+
 const userResetPassword = async (request, response) => {
     const { token, newPassword, confirmPassword } = request.body
 
     const user = await UserModel.findOne({ resetToken: token })
     if (!user) {
-        return response.status(404).json({ message: 'Invalid or expired reset token!' })
+        return response.status(404).json({ message: 'Invalid or expired reset token!'})
     }
-
     if (Date.now() > user.tokenExpiration) {
-        return response.status(400).json({ message: 'Reset token has expired!' })
+        return response.status(400).json({ message: 'Reset token has expired!'})
     }
-
+    if (newPassword.length<6) {
+        return response.status(400).json({ message: 'Passwords must have atleast 6 characters!'})
+    }
     if (newPassword !== confirmPassword) {
-        return response.status(400).json({ message: 'Passwords do not match!' })
+        return response.status(400).json({ message: 'Passwords do not match!'})
     }
 
     user.password = newPassword 
@@ -96,39 +148,6 @@ const userResetPassword = async (request, response) => {
     return response.status(200).json({ message: 'Password reset successful!' })
 }
 
-const forgotPassword = async (request, response) => {
-    const { email } = request.body
-
-    const user = await UserModel.findOne({ email })
-    if (!user) {
-        return response.status(404).json({ message: 'Account not found!' })
-    }
-
-    const resetToken = generateResetToken()
-    const tokenExpiration = Date.now() + 3600000
-
-    user.resetToken = resetToken
-    user.tokenExpiration = tokenExpiration
-    await user.save()
-
-    const resetLink = `https://yourfrontendurl.com/reset-password?token=${resetToken}`
-
-    const msg = {
-        to: email,
-        from: 'honeycoupleart@gmail.com',
-        subject: 'Password Reset Request',
-        text: `Click the link below to reset your password (this link will expire in 1 hour):\n\n${resetLink}`,
-        html: `<p>Click the link below to reset your password (this link will expire in 1 hour):</p><a href="${resetLink}">Reset Password</a>`
-    }
-
-    try {
-        await sgMail.send(msg)
-        return response.status(200).json({ message: 'Password reset email sent!' })
-    } catch (error) {
-        console.error('Error sending email:', error);
-    response.status(500).json({ message: 'Failed to send reset email. Please try again later.' });
-    }
-}
 
 
 module.exports = { userSignUp, userLogin, userResetPassword, forgotPassword }
